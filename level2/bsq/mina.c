729523b3cc68b0f12a680323a6d47da4cf40f792
mina-houshmand <houshmand_mina@yahoo.com>
2026-03-12 21:04:10 +0100

    ad

--- level2/bsq/mina.c
+++ level2/bsq/mina.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>  //**
 
 typedef struct{
 int rows;
@@ -21,39 +21,37 @@ void free_map(Map *m){
 	free(m);
 }
 
+int min3(int a, int b, int c){
+	return(a < b ? ( a < c ? a : c ) : (b < c ? b : c));
+}
+
 int validate(const Map *m){
 	if (!m || m->rows < 1 || m->cols < 1)
 		return 0;
-	if (m->empty == m->obs || m->empty == m->fill || m->obs == m->fill)
+	if (m->empty == m->obs || m->empty == m->obs || m->obs == m->fill)
 		return 0;
 	for (int i = 0; i < m->rows; i++){
-		for(int j = 0; j < m->cols; j++)
-			if (m->grid[i][j] != m->empty && m->grid[i][j] != m->obs)   //&&
+		for (int j = 0; j < m->cols; j++){
+			if (m->grid[i][j] != m->empty && m->grid[i][j] != m->obs)
 				return 0;
+		}	
 	}
 	return 1;
 }
 
-int min3(int a, int b, int c){
-	return ( a < b ? ( a < c ? a : c ) : ( b < c ? b : c));
-}
-
-Map *read_map(const char *av){
-	FILE *f = av ? fopen(av, "r") : stdin;
+Map *read_map(char *av){
+	FILE *F = av ? fopen(av, "r") : stdin;
 	if (f == 0)
 		return NULL;
-		
-	//map allocation
 	Map *m = calloc(1, sizeof(Map));
-	if(fscanf(f, "%d %c %c %c\n", &m->rows, &m->empty, &m->obs, &m->fill) != 4)  //fscanf
+	if (fscanf(f, "%d %c %c %c\n", &m->rows, &m->empty, &m->obs, &m->fill) != 4)
 		goto err;
-	//grid allocation
 	m->grid = malloc(m->rows * sizeof(char*));
-	for(int i = 0; i < m->rows; i++){
-		size_t len =0;  //forgot
-		if (getline(&m->grid[i], &len, f) < 0)
+	for (int i = 0; i < m->rows; i++){
+		size_t len = 0;
+		if (getline(&m->grid[i], &len, f) < 0){
 			goto err;
-			
+		}
 		int collen = 0;
 		while (m->grid[i][collen] && m->grid[i][collen] != '\n')
 			collen++;
@@ -62,11 +60,11 @@ Map *read_map(const char *av){
 			m->cols = collen;
 		else if (m->cols != collen)
 			goto err;
-		
 	}
+
 	if (f != stdin)
 		fclose(f);
-	return m;
+	return m;	
 err:
 	if (f != stdin)
 		fclose(f);
@@ -77,60 +75,56 @@ err:
 void solve(Map *m){
 	int R = m->rows;
 	int C = m->cols;
-	int **dp = calloc(R, sizeof(*dp));  //int
+	int **dp = calloc(R, sizeof(*dp));
 	
-	for (int i = 0; i < R; i++)
+	for (int i = 0; i < m->rows; i++){
 		dp[i] = calloc(C, sizeof(*dp[i]));
 	
 	int best = 0;
 	int bi = 0;
 	int bj = 0;
 	
-	for ( int i = 0; i < R; i++){
-		for (int j = 0; j < C; j++){
+	for (int i = 0; i < m->rows; i++){
+		for (int j = 0; j < m->cols; j++){
 			if (m->grid[i][j] == m->obs)
 				dp[i][j] = 0;
 			else
-				dp[i][j] = (i && j ? min3(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) : 0 ) + 1;  //dp
+				dp[i][j] = ( i && j ? min3(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) : 0 ) + 1;
 			if (dp[i][j] > best){
 				best = dp[i][j];
 				bi = i;
-				bj = j; 
+				bj = j;
 			}
 		}
-	}  //ino beband
-	for (int i = bi - best + 1; i <= bi; i++){
-		for (int j = bj - best + 1; j <= bj; j++){
-			m->grid[i][j] = m->fill;  //forgot
+	}
+	for(int i = bi - best + 1; i < bi; i++){
+		for (int j = bj - best + 1; j < bj; j++){
+			
 		}
 	}
-	
-	for (int i = 0; i < R; i++)   //forgot
-		free(dp[i]);
-	free(dp);
-
-
 }
+	
 
-void	process(char *av){
+void process(const char *av){
 	Map *m = read_map(av);
-	if (!m || !validate(m)){
+	if (!m || !validate(av)){
 		fprintf(stderr, "error map\n");
 		free_map(m);
 		return;
 	}
 	solve(m);
-	for(int i = 0; i < m->rows; i++){
+	for (int i = 0; i < m->rows; i++){
 		fprintf(stdout, "%s\n", m->grid[i]);
 	}
 	free_map(m);
 }
 
+
 int main(int ac, char **av){
 	if (ac < 2)
 		process(NULL);
 	else{
-		for (int i = 1; i < ac; i++)  //i = 1
+		for( int i = 1; i < ac; i++)
 			process(av[i]);
 	}
 	return 0;
--- level2/game_of_life/mina.c
+++ level2/game_of_life/mina.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 
 int main(int ac, char **av){
 	(void)ac;
@@ -18,45 +19,49 @@ int main(int ac, char **av){
 	char c;
 	
 	for (int i = 0; i < h; i++){
-		for(int j = 0; j < w; j++){
+		for(int j= 0; j < w; j++){
 			board[i][j] = 0;
 		}
 	}
 	
 	while(read(0, &c, 1) == 1){
 		if( c == 'w' && y > 0) y--;
-		else if( c == 's' && y < h -1) y++;
-		else if( c == 'a' && x > 0) x--;
-		else if( c == 'd' && x < w - 1) x++;
-		else if( c == 'x') pen = !pen;
-		if (pen) board[y][x] = 1;
+		else if(c == 's' && y < h - 1) y++;
+		else if(c == 'a' && x > 0) x--;
+		else if(c == 'd' && x < w - 1) x++;
+		else if(c == 'x') pen = !pen;
+		if (pen)
+			board[y][x] = 1;
 	}
 	
 	for(int t = 0; t < iter; t++){
 		for (int i = 0; i < h; i++){
-			for(int j = 0; j < w; j++){
+			for(int j= 0; j < w; j++){
 				int n = 0;
 				for(int di = -1; di <= 1; di++){
-				  for (int dj = -1; dj <= 1; dj++){
-			            if((di || dj) && di + i >= 0 && di + i < h && dj + j >= 0 && dj + j < w)
-					n += (board[di + i][di + j]);
-				  }
+					for(int dj = -1; dj <= 1; dj++){
+						if ((di || dj) && di + i >= 0 && di + i < h && dj + j >= 0 && dj + j < w)
+							n += board[di + i][dj + j];
+					}
 				}
 				next[i][j] = (board[i][j] && ( n == 2 || n == 3)) || (!board[i][j] && n == 3);
 			}
 		}
-		for (int i = 0; i < h; i++){
-			for(int j = 0; j < w; j++){
-				board[i][j] = next[i][j];
-			}
+	
+	for (int i = 0; i < h; i++){  //yadet nare in dakhele iteretiane
+		for(int j= 0; j < w; j++){
+			board[i][j] = next[i][j];
 		}
 	}
+}
 	
 	for (int i = 0; i < h; i++){
-		for(int j = 0; j < w; j++){
-			putchar(board[i][j] ? '0' : ' ');
-		}
+		for(int j= 0; j < w; j++){
+			putchar(board[i][j] ? '0' : ' ');}
 		putchar('\n');
+		
 	}
+	
+	
 	return 0;
 }
